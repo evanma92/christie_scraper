d6d33861b15347fb52331682697c5912ea53b7bf
evanma92 <evanma92@gmail.com>
2017-12-12 22:08:20 +0800

    Removed sothesby elements due to scraping ban

--- tutorial/tutorial/spiders/art_spider.py
+++ tutorial/tutorial/spiders/art_spider.py
@@ -5,17 +5,9 @@ from scrapy.http import Request
 import scrapy
 
 from selenium_christie import required_links
-from selenium_sothesby import sothesby_links
+
 
 class ChristiesItem(Item):
-    artist = Field()
-    title = Field()
-    price_realised = Field()
-    lot_number = Field()
-    low_estimate = Field()
-    high_estimate = Field()
-
-class SothesbyItem(Item):
     artist = Field()
     title = Field()
     price_realised = Field()
@@ -45,29 +37,3 @@ class ChristiesSpider(Spider):
         item['low_estimate'] = prices[0]
         item['high_estimate'] = prices[1]
         return item
-
-
-class SothesbySpider(Spider):
-    name = 'sothesby'
-    allowed_urls = ['http://www.sothebys.com/']
-    start_urls = sothesby_links
-
-
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    
-    def parse(self, response):
-        item = SothesbyItem()
-        item['artist'] = response.css("div.lotdetail-guarantee::text").extract()
-        item['price_realised'] = response.css("div.price-sold::text").extract()
-        item['lot_number'] = response.css("div.lotdetail-lotnumber::text").extract()
-        item['title'] = response.css("div.lotdetail-subtitle::text").extract()
-        return item
-
-        
-
-
-
-
