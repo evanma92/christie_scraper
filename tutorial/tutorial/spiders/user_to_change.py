e27f6ab5087e42edcf82a0f9df9cf1eb58e78c33
evanma92 <evanma92@gmail.com>
2017-12-12 22:30:39 +0800

    Wrote instructions for using scraper

--- tutorial/tutorial/spiders/user_to_change.py
+++ tutorial/tutorial/spiders/user_to_change.py
@@ -1,5 +1,5 @@
 # change username under <insert username>
-chrome_driver = '/Users/evanma/Downloads/chromedriver'
+chrome_driver = '/Users/<insert username>/Downloads/chromedriver'
 
 # change url to christie's auction you want to scrape
 url = 'http://www.christies.com/salelanding/index.aspx?intsaleid=26796&saletitle='
